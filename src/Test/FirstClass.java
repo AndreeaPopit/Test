a8ba7ddfed731600744c8ec7cb2e0489677efbe7
Andreea Popit <popitandreea@gmail.com>
2014-10-25 20:05:17 +0300

    Test project commit

--- src/Test/FirstClass.java
+++ src/Test/FirstClass.java
@@ -4,20 +4,20 @@ package Test;
  * Created by Andreea on 25.10.2014.
  *
  * Print salary based on pay and hours worked
- *
  */
 public class FirstClass {
-    private static void pay(int WorkedHours, int Overtime, double MinimWage, double Wage, int MaxWork){
-        if (Wage < MinimWage || (WorkedHours+Overtime) > MaxWork){
+    private static void pay(int WorkedHours, int Overtime, double MinimWage, double Wage, int MaxWork) {
+        if (Wage < MinimWage || (WorkedHours + Overtime) > MaxWork) {
             System.out.println("Your boss must be sued!");
-        }
-        else{
-            double salary ;
-            salary = Wage*WorkedHours + (1.5*Wage)*Overtime;
+        } else {
+            double salary;
+            salary = Wage * WorkedHours + (1.5 * Wage) * Overtime;
             System.out.println("Your salary is " + salary);
         }
     }
-    public static void main(String[] arguments){
-        pay(20,10,8.0,10.0,60);
+
+    public static void main(String[] arguments) {
+        pay(20, 10, 8.0, 10.0, 60);
+        pay(70,40,6,7,50);
     }
 }
